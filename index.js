1f77c4d70d6053391a088ec23328c0d55bdaecb7
RassielMex <rhcp94.rm@gmail.com>
2022-04-09 10:46:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+calculatedTitleMargin();
+$(window).on("resize", calculatedTitleMargin);
+
+function calculatedTitleMargin() {
+  let cardContainerWidth = parseInt($(".card-container").css("width"));
+  let cardWidth = parseInt($(".card").css("width"));
+  const gap = 0.2 * 16;
+
+  let maxCards = Math.floor(cardContainerWidth / cardWidth);
+  let content = cardWidth * maxCards + (maxCards - 1) * gap;
+  let calculatedMargin = Math.floor((cardContainerWidth - content) / 2);
+  // console.log(content);
+  // console.log(calculatedMargin);
+  $(".section-title").css("marginLeft", calculatedMargin);
+}
+
+$(".card").on("click", function () {
+  let netflixSound = new Audio("./resources/sounds/Netflix-Intro-Sound.mp3");
+  if (netflixSound) {
+    netflixSound.play();
+  }
+  setTimeout(() => {
+    $("main").addClass("noDisplay");
+    // document.location.href = "./pages/details.html";
+  }, 4000);
+});
+
+$("nav").on("click", function () {
+  $("main").removeClass("noDisplay");
+});
